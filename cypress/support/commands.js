5c028342274a830adea3d39b6bc1771f44be1e62
Alecio L. Medeiros <Alexdesaran@outlook.com>
2023-09-11 13:56:03 -0300

    Break the app on purpose
    Break the app on purpose

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,6 +4,6 @@ Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function(){
     cy.get('#email').type('Alecio@email.com')
     cy.get('#phone').type('4399999999')
     cy.get('#open-text-area').type('otimo curso!!!')
-    cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
     cy.get('.success').should('be.visible')
 })
